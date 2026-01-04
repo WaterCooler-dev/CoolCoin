46588654b27216f08a39e792445fffe51a49e5a2
kmskr11 <minsung77211@gmail.com>
2026-01-04 23:52:17 +0900

    netty 버그 수정

--- src/main/java/dev/watercooler/coolcoin/P2P/P2PHandler.java
+++ src/main/java/dev/watercooler/coolcoin/P2P/P2PHandler.java
@@ -1,8 +1,10 @@
 package dev.watercooler.coolcoin.P2P;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -10,14 +12,14 @@ public class P2PHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
     protected void messageReceived(ChannelHandlerContext ctx, ByteBuf byteBuf) {
         log.debug("P2P를 통해 특정 값을 전달받음");
-        String context = byteBuf.toString();
-        ctx.writeAndFlush("값 받음" + context);
+        String context = byteBuf.toString(CharsetUtil.UTF_8);
+        ctx.writeAndFlush(Unpooled.copiedBuffer("값 받음" + context, CharsetUtil.UTF_8));
         ctx.close();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
-        cause.printStackTrace();
+        log.error("P2P 핸들러 에러 발생", cause);
         ctx.close();
     }
 }
--- src/test/java/P2P/P2PTestClient.java
+++ src/test/java/P2P/P2PTestClient.java
@@ -2,15 +2,17 @@ package P2P;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.CharsetUtil;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class P2PTestClient {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         String host = "127.0.0.1";
         int port = 8080;
         EventLoopGroup workerGroup = new NioEventLoopGroup();
@@ -22,17 +24,20 @@ public class P2PTestClient {
             b.option(ChannelOption.SO_KEEPALIVE, true); // (4)
             b.handler(new ChannelInitializer<SocketChannel>() {
                 @Override
-                public void initChannel(SocketChannel ch) throws Exception {
+                public void initChannel(SocketChannel ch){
                     ch.pipeline().addLast(new P2PTestHandler());
                 }
             });
 
             ChannelFuture f = b.connect(host, port).sync();
-            f.channel().writeAndFlush("Hello, Netty Server!\n");
-
-            if (f.isSuccess()) {
-                log.debug("메세지 보냄");
-            }
+            f.channel().writeAndFlush(Unpooled.copiedBuffer("Hello, Netty Server!\n", CharsetUtil.UTF_8))
+                    .addListener((ChannelFutureListener) future -> {
+                        if (!future.isSuccess()) {
+                            log.error("메세지 전송 실패", future.cause());
+                        } else {
+                            log.debug("메세지 보냄");
+                        }
+                    });
 
             f.channel().closeFuture().sync();
         } catch (Exception e) {
@@ -46,14 +51,14 @@ public class P2PTestClient {
 @Slf4j
 class P2PTestHandler extends SimpleChannelInboundHandler<ByteBuf> {
     @Override
-    protected void messageReceived(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
-        log.info("서버한테 데이터 성공적으로 전송받음: {}", byteBuf.toString());
+    protected void messageReceived(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf){
+        log.debug("서버한테 데이터 성공적으로 전송받음: {}", byteBuf.toString(CharsetUtil.UTF_8));
         channelHandlerContext.close();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-        cause.printStackTrace();
+        log.error("클라이언트 핸들러 에러 발생", cause);
         ctx.close();
     }
 }
